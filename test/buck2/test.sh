c892cb9b1873931efc407be1be0bb5abc6edce49
0xG0nz0 <8682922+0xg0nz0@users.noreply.github.com>
2024-01-29 00:44:17 +0000

    More test cleanup

--- test/buck2/test.sh
+++ test/buck2/test.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
 
-# This test file will be executed against an auto-generated devcontainer.json that
-# includes the 'hello' Feature with no options.
-#
-# For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
-#
-# Eg:
-# {
-#    "image": "<..some-base-image...>",
-#    "features": {
-#      "hello": {}
-#    },
-#    "remoteUser": "root"
-# }
-#
-# Thus, the value of all options will fall back to the default value in 
-# the Feature's 'devcontainer-feature.json'.
-# For the 'hello' feature, that means the default favorite greeting is 'hey'.
-#
-# These scripts are run as 'root' by default. Although that can be changed
-# with the '--remote-user' flag.
-# 
 # This test can be run with the following command:
 #
 #    devcontainer features test \ 
-#                   --features hello   \
+#                   --features buck2   \
 #                   --remote-user root \
 #                   --skip-scenarios   \
 #                   --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
