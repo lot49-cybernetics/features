daf89f2db3e6c31a6af6c64fb554f4bcec6551b6
0xG0nz0 <8682922+0xg0nz0@users.noreply.github.com>
2024-01-29 00:56:39 +0000

    Another attempt to fix tests

---
+++ test/buck2/buck2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This test can be run with the following command:
+#
+#    devcontainer features test \ 
+#                   --features buck2   \
+#                   --remote-user root \
+#                   --skip-scenarios   \
+#                   --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
+#                   /path/to/this/repo
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
+# Provides the 'check' and 'reportResults' commands.
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib. Syntax is...
+# check <LABEL> <cmd> [args...]
+check "execute command" bash -c "hello | grep 'hello'"
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
